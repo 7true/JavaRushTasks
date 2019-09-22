8230cd7ded603ace8284f6abe4d766f5a4dc7420
alx <37true@gmail.com>
2019-09-22 19:26:49 +0700

    checkit

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -1,14 +1,16 @@
+package com.javarush.task.task18.task1828;
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
-        if (args.length == 4 && args[0].equals("-c")) {
+        if ((args.length == 5 || args.length == 2) && (args[0].equals("-u") || args[0].equals("-d"))) {
             try {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                 String fileName = br.readLine();
                 BufferedReader brf = new BufferedReader(new FileReader(fileName));
-                BufferedWriter fbw = new BufferedWriter(new FileWriter(fileName, true));
+
                 ArrayList<String> arrString = new ArrayList<>();
 
                 String line;
@@ -16,43 +18,60 @@ public class Solution {
                     arrString.add(line);
                 }
 
-                int currentId = 0;
-                int maxId = 0;
-                for (String s : arrString) {
-                    currentId = Integer.parseInt(s.substring(0, 8).trim());
-                    if (currentId > maxId)
-                        maxId = currentId;
+                int idUpdate = Integer.parseInt(args[1]);
+                int currentId;
+                StringBuilder sb = new StringBuilder();
+                if (args.length == 5) {
+                    sb.append(args[1]);
+                    sb = sbAppendSpaces(sb, 8);
+                    sb.append(args[2]);
+                    sb = sbAppendSpaces(sb, 38);
+                    sb.append(args[3]);
+                    sb = sbAppendSpaces(sb, 46);
+                    sb.append(args[4]);
+                    sb = sbAppendSpaces(sb, 50);
+                }
+                String stringReplacement = sb.toString();
+                brf.close();
+                BufferedWriter fbw = new BufferedWriter(new FileWriter(fileName));
+                //update
+                if (args[0].equals("-u")) {
+                    for (int i = 0; i < arrString.size(); i++) {
+                        if (Integer.parseInt(arrString.get(i).substring(0, 8).trim()) == idUpdate) {
+                            fbw.write(stringReplacement);
+                            fbw.write("\n");
+                            continue;
+                        }
+                        fbw.write(arrString.get(i));
+                        if (i == arrString.size() - 1) break;
+                        fbw.write("\n");
+                    }
                 }
 
-                StringBuilder sb = new StringBuilder();
-                sb.append("\n");
-                sb.append(++maxId);
-                while (sb.length() < 9)
-                    sb.append(" ");
-                String productName = args[1];
-                String price = args[2];
-                String quantity = args[3];
-                sb.append(productName);
-                while (sb.length() < 39)
-                    sb.append(" ");
+                //delete
+                if (args[0].equals("-d")) {
+                    for (int i = 0; i < arrString.size(); i++) {
+                        if (Integer.parseInt(arrString.get(i).substring(0, 8).trim()) == idUpdate) {
+                            continue;
+                        }
+                        fbw.write(arrString.get(i));
+                        if (i == arrString.size() - 1) break;
+                        fbw.write("\n");
+                    }
+                }
 
-                sb.append(price);
-                while (sb.length() < 47)
-                    sb.append(" ");
-                sb.append(quantity);
-                while (sb.length() < 51)
-                    sb.append(" ");
-
-                String s = sb.toString();
-                br.close();
-
-                fbw.write(s);
-                brf.close();
                 fbw.close();
+                br.close();
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+    }
+    public static StringBuilder sbAppendSpaces(StringBuilder sb, int size) {
+        while (sb.length() < size)
+            sb.append(" ");
+        return sb;
     }
 }
