5c486dff7fd325adb50805a15fb6d256d7ba8758
alx <37true@gmail.com>
2019-09-29 19:52:52 +0700

    checkit

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task19.task1916;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +14,18 @@ public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
     public static void main(String[] args) {
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String fileName1 = br.readLine();
+            String fileName2 = br.readLine();
+
+
+            ArrayList<String> lines = new ArrayList();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
