9537c2247f245658e54733cfaa84a12902e6350b
alx <37true@gmail.com>
2019-10-04 15:35:41 +0700

    checkit

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task19.task1916;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-/* 
+/*
 Отслеживаем изменения
 */
 
@@ -16,6 +19,7 @@ public class Solution {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String fileName1 = br.readLine();
             String fileName2 = br.readLine();
+            br.close();
 
             BufferedReader br1 = new BufferedReader(new FileReader(fileName1));
             BufferedReader br2 = new BufferedReader(new FileReader(fileName2));
@@ -33,12 +37,12 @@ public class Solution {
 
             Collections.reverse(file1);
             Collections.reverse(file2);
-            int size1 = file1.size();
-            int size2 = file2.size();
+
             String s1, s2, next;
-            for(int i = 0; file1.size () > 0 && file2.size() > 0; i++) {
+            for(; file1.size () > 1 || file2.size() > 1;) {
                 s1 = file1.pop();
                 s2 = file2.pop();
+
                 if (s1.equals(s2)) {
                     lines.add(new LineItem(Type.SAME,s1));
                 }
@@ -52,19 +56,32 @@ public class Solution {
                         file1.push(next);
                         next = file2.pop();
                         if (next.equals(s1)) {
-                            lines.add(new LineItem(Type.REMOVED, s2));
+                            lines.add(new LineItem(Type.ADDED, s2));
                             lines.add(new LineItem(Type.SAME, next));
                         }
-                        file2.push(next);
                     }
                 }
+
+                if (file1.size() > 0 && file2.size() == 0) {
+                    while (file1.size () > 0)
+                        lines.add(new LineItem(Type.REMOVED, file1.pop()));
+                }
+
+                if (file1.size() == 0 && file2.size() > 0) {
+                    while (file2.size () > 0)
+                        lines.add(new LineItem(Type.ADDED, file2.pop()));
+                }
+
+                br1.close();
+                br2.close();
                 //System.out.println(file1.pop());
             }
-            ArrayList<String> lines = new ArrayList();
-
-            while (lines.listIterator().hasNext()) {
-                System.out.println(lines.iterator().next());
-            }
+            //ArrayList<String> lines = new ArrayList();
+            /*Iterator<LineItem> lIt = lines.iterator();
+            while (lIt.hasNext()) {
+                LineItem current = lIt.next();
+                System.out.println(current.type + " " + current.line);
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -72,9 +89,9 @@ public class Solution {
 
 
     public static enum Type {
-        ADDED,        //добавлена новая строка
-        REMOVED,      //удалена строка
-        SAME          //без изменений
+        ADDED, //добавлена новая строка
+        REMOVED, //удалена строка
+        SAME //без изменений
     }
 
     public static class LineItem {
