18c40733146926d1272c44ceb70ef63f3b84936a
alx <37true@gmail.com>
2019-11-04 23:11:42 +0700

    checkit

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -15,7 +15,10 @@ public class Solution {
                 {'m', 'l', 'p', 'r', 'r', 'h'},
                 {'p', 'o', 'e', 'e', 'j', 'j'}
         };
-        detectAllWords(crossword, "home", "same");
+        List<Word> words = detectAllWords(crossword, "home", "same");
+        for (Word w : words) {
+            System.out.println(w);
+        }
         /*
 Ожидаемый результат
 home - (5, 3) - (2, 0)
@@ -23,49 +26,66 @@ same - (1, 1) - (4, 1)
          */
     }
 
-    public static List<Word> detectAllWords(int[][] crossword, String... words) {
-        ArrayList <String> arrWordsHorizont = new ArrayList();
-        ArrayList <String> arrWordsVertical = new ArrayList();
-        ArrayList <String> arrWordsDiagDown = new ArrayList();
-        ArrayList <String> arrWordsDiagUp = new ArrayList();
-        StringBuilder sbH = new StringBuilder();
-        StringBuilder sbV = new StringBuilder();
-        StringBuilder sbD = new StringBuilder();
-        for (int i = 0; i < crossword.length; i++) {
-            for (int j = 0; j < crossword[i].length; j++) {
-                sbH.append((char)crossword[i][j]);
-            }
-            arrWordsHorizont.add(sbH.toString());
-            sbH.delete(0, sbH.length());
-            for (int k = 0; k < crossword.length; k++) {
-                sbV.append((char) crossword[k][i]);
-            }
-            arrWordsVertical.add(sbV.toString());
-            sbV.delete(0, sbV.length());
-            for (int r = 0; i + r < crossword.length; r++) {
-                sbD.append((char) crossword[i+r][r]);
-            }
-            arrWordsDiagDown.add(sbD.toString());
-            sbD.delete(0, sbD.length());
-            for (int r = 0; i + r < crossword.length; r++) {
-                sbD.append((char) crossword[r][i+r]);
-            }
-            arrWordsDiagDown.add(sbD.toString());
-            sbD.delete(0, sbD.length());
-        }
+    static Word searchWord(int[][] grid, int row,
+                            int col, String word) {
+        int R, C, eR = 0, eC = 0;
+        int[] x = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] y = { -1, 0, 1, -1, 1, -1, 0, 1 };
+        R = grid.length;
+        C = grid[0].length;
+        if (grid[row][col] != word.charAt(0))
+            return null;
 
-        for (String s : arrWordsHorizont) {
-            System.out.println(s);
-        }
-        System.out.println("########");
-        for (String s : arrWordsVertical) {
-            System.out.println(s);
-        }
-        System.out.println("########");
-        for (String s : arrWordsDiagDown) {
-            System.out.println(s);
+        int len = word.length();
+
+        for (int dir = 0; dir < 8; dir++) {
+            int k, rd = row + x[dir], cd = col + y[dir];
+
+            for (k = 1; k < len; k++) {
+                if (rd >= R || rd < 0 || cd >= C || cd < 0)
+                    break;
+                if (grid[rd][cd] != word.charAt(k))
+                    break;
+                eR = rd;
+                eC = cd;
+                rd += x[dir];
+                cd += y[dir];
+
+            }
+
+            // word found!
+            if (k == len) {
+                Word w = new Word(word);
+                w.setStartPoint(col, row);
+                w.setEndPoint(eC, eR);
+                return w;
+            }
         }
         return null;
+    }
+
+    static Word patternSearch(int[][] grid, String word) {
+        Word w;
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                w = searchWord(grid, row, col, word);
+                if (w != null) {
+                    return w;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static List<Word> detectAllWords(int[][] crossword, String... words) {
+
+        List<Word> wordList = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            Word w = patternSearch(crossword, words[i]);
+            if (w != null)
+                wordList.add(w);
+        }
+        return wordList;
     }
 
     public static class Word {
