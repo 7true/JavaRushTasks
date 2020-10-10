a3bff3f43a8e0e9a6894e2595803beaf65f6ed95
alx <37true@gmail.com>
2020-10-10 18:24:56 +0700

    finished

--- 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -11,16 +11,13 @@ import java.util.regex.Pattern;
 Рекурсия для мат. выражения
 */
 public class Solution {
-    private final String[] FUNCTIONS = {"abs", "acos", "arg",
-    "atan", "conj", "cos","cosh", "exp",
-    "imag", "log", "neg", "pow", "real",
-    "sin", "sinh", "sqrt", "tan", "tanh"};
-
-    private final String OPERATORS = "+-*/";
+    private final String[] FUNCTIONS = {"s", "c", "t"};
+    private final String FUNCTIONS_STRING = "sct";
+    private final String OPERATORS = "+-*/^";
     private final String VARIABLE = "var";
     private final String SEPARATOR = ",";
     private Stack<String> StackOperations = new Stack<String>();
-    private Stack<String> StackOPN = new Stack<String>();
+    private Stack<String> StackRPN = new Stack<String>();
     private Stack<String> stackAnswer = new Stack<String>();
 
     public Solution() {
@@ -66,30 +63,144 @@ public class Solution {
         return OPERATORS.contains(token);
     }
 
+    private static String evalRPN(ArrayList<String> expr){
+        LinkedList<Double> stack = new LinkedList<Double>();
+        System.out.println("Input\tOperation\tStack after");
+        for (String token : expr){
+            System.out.print(token + "\t");
+            if (token.equals("*")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand * secondOperand);
+            } else if (token.equals("/")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand / secondOperand);
+            } else if (token.equals("-")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand - secondOperand);
+            } else if (token.equals("+")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand + secondOperand);
+            } else if (token.equals("^")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(Math.pow(firstOperand, secondOperand));
+            } else if (token.equals("s")) {
+                double firstOperand = stack.pop();
+                stack.push(Math.sin(Math.toRadians(firstOperand)));
+            }
+            else if (token.equals("c")) {
+                double firstOperand = stack.pop();;
+                stack.push(Math.cos(firstOperand));
+            }
+            else if (token.equals("t")) {
+                double firstOperand =  stack.pop();;
+                stack.push(Math.tan(firstOperand));
+            }
+            else {
+                System.out.print("Push\t\t");
+                try {
+                    stack.push(Double.parseDouble(token+""));
+                } catch (NumberFormatException e) {
+                    System.out.println("\nError: invalid token " + token);
+                    return null;
+                }
+            }
+            System.out.println(stack);
+        }
+        if (stack.size() > 1) {
+            System.out.println("Error, too many operands: " + stack);
+            return null;
+        }
+        System.out.println("Final answer: " + stack.peek());
+        return stack.pop().toString();
+    }
+
     private byte getPrecendence(String token) {
         if (token.equals("+") || token.equals("-")) {
             return 1;
+        } else if (token.equals("^")) {
+            return 3;
+        } else if (token.equals("s") || token.equals("c") || token.equals("t")) {
+            return 4;
         }
         return 2;
     }
 
+    private String calcRPN(String exp) {
+        Stack<String> operations = new Stack<>();
+        ArrayList<String> output = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(exp, FUNCTIONS_STRING + OPERATORS + SEPARATOR  + "()", true);
+        System.out.println(exp);
+        int indexLastOpen = exp.indexOf("(");
+        int indexfirstClose = exp.lastIndexOf(")");
+        if (indexLastOpen == -1) {
+            System.out.println("calc nested exp with RPN");
+            output.clear();
+            while (st.hasMoreTokens()) {
+                String token = st.nextToken();
+                if (isFunction(token)) {
+                    operations.push(token);
+                }
+                if (isOperator(token)) {
+                    int precendenceToken = getPrecendence(token);
+                    if (!operations.isEmpty()) {
+                        int precedenceFromStack = getPrecendence(operations.peek());
+                        if (precendenceToken <= precedenceFromStack || isFunction(operations.peek())) {
+                            output.add(operations.pop());
+                        }
+                    }
+                    operations.push(token);
+                }
+                if (isNumber(token)) {
+                    output.add(token);
+                }
+            }
+
+            while (!operations.isEmpty()) {
+                output.add(operations.pop());
+            }
+            System.out.println("*********");
+            for(String i : output) {
+                System.out.print(i);
+            }
+            System.out.println();
+            System.out.println("*********");
+            return evalRPN(output);
+        } else {
+            String substrToRPN = exp.substring(indexLastOpen + 1, indexfirstClose);
+            String expression = exp.substring(0, indexLastOpen) + calcRPN(substrToRPN) + exp.substring(indexfirstClose + 1);
+//            if (exp.substring(0,1).equals("s")) {
+//                expression = exp.substring(0,1) + substrToRPN;
+//                System.out.println(expression);
+//            }
+//            long count = exp.chars().filter(ch -> ch == '(').count();
+//            if (count == 1 && exp.substring(0,1).equals("s")) {
+//                expression = "s" + expression;
+//            }
+//            calcRPN(expression);
+            System.out.println(expression);
+            return calcRPN(expression);
+        }
+    }
+
     public void recurse(final String expression, int countOperation) {
         //implement
-        String expressionL = expression.replace(" ", "").replace("(-", "(0-")
-                .replace(",-", ",0-");
-
-        Stack<Double> currentRes = new Stack<>();
-        StringTokenizer st = new StringTokenizer(expressionL,
-                OPERATORS + SEPARATOR  + "()", true);
-        System.out.println(st);
-
-        String str = expressionL.replaceFirst("\\(", "");
-        String[] results = str.split("\\)")[0].split("\\(");
-
-        for (String string : results) {
-            System.out.println(string);
-        }
-
+        String expressionL = expression.replace(" ", "")
+                .replace("-", "0-").replace("sin", "s").replace("cos","c")
+                .replace("tan", "t");
+       /* String expressionL = expression.replace(" ", "").replace("(-", "(0-")
+                .replace(",-", ",0-");*/
+        calcRPN(expressionL);
+/*
         int l = results.length;
         for (int j = 0; j < l / 2; j++) {
             String temp = results[j];
@@ -102,5 +213,7 @@ public class Solution {
             String token = st.nextToken();
             System.out.println(token);
         }
+        */
+
     }
 }
